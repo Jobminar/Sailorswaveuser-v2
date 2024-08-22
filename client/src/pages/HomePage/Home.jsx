c23906c228151da4e24e7ce0c1e0ca8cea29367f
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-08-22 10:37:36 +0530

    added the package-lock.json

--- client/src/pages/HomePage/Home.jsx
+++ client/src/pages/HomePage/Home.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import Navbar from '../../components/Header/Navbar.jsx'
-import Header from '../../components/Header/Header.jsx'
-import WhyChooseSailorsWave from '../../components/WhyChooseSailorsWave/WhyChooseSailorsWave.jsx'
-import TrustedByCompanies from '../../components/TrustedByCompanies/TrustedByCompanies.jsx'
-import OurServices from '../../components/OurServices/OurServices.jsx'
-import OurClients from '../../components/OurClients/OurClients.jsx'
-import BecomeASailor from '../../components/BecomeASailor/BecomeASailor.jsx'
-import Footer from '../../components/Footer/Footer.jsx'
-import AboutUs from '../../components/AboutUs/AboutUs.jsx'
+import React from "react";
+import Navbar from "../../components/Header/Navbar.jsx";
+import Header from "../../components/Header/Header.jsx";
+import WhyChooseSailorsWave from "../../components/WhyChooseSailorsWave/WhyChooseSailorsWave.jsx";
+import TrustedByCompanies from "../../components/TrustedByCompanies/TrustedByCompanies.jsx";
+import OurServices from "../../components/OurServices/OurServices.jsx";
+import OurClients from "../../components/OurClients/OurClients.jsx";
+import BecomeASailor from "../../components/BecomeASailor/BecomeASailor.jsx";
+import Footer from "../../components/Footer/Footer.jsx";
+import AboutUs from "../../components/AboutUs/AboutUs.jsx";
 
 const HomePage = () => {
   return (
     <>
-    {/* <Header /> */}
-    {/* <Navbar /> */}
-    {/* <AboutUs /> */}
-    {/* <WhyChooseSailorsWave/> */}
-    {/* <TrustedByCompanies/> */}
-    {/* <OurServices/> */}
-    {/* <OurClients/> */}
-    <BecomeASailor/>
-    {/* <Footer/> */}
-
+      <Header />
+      <Navbar />
+      <AboutUs />
+      <WhyChooseSailorsWave />
+      <TrustedByCompanies />
+      <OurServices />
+      <OurClients />
+      {/* <BecomeASailor/> */}
+      <Footer />
     </>
-
-  )
-}
+  );
+};
 
 export default HomePage;
