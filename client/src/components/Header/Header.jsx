f4ccf945ea81fd4a6cf3cebeb01fcab1d47bf497
anilvarmathirumalaraju <anilvarmathirumalaraju@gmail.com>
2024-08-28 17:14:40 +0530

    chnages done in mobile nav bar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import phoneIcon from "../../assets/Phone-icon.png";
 import "./Header.css";
 import logo from "../../assets/Sailors-Logo.png";
 import { useNavigate } from "react-router-dom";
+import { HashLink} from 'react-router-hash-link';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -13,18 +14,9 @@ const Header = () => {
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
-    const mobileNav = document.querySelector('.bar');
-    if (!isOpen) {
-      mobileNav.classList.add('open');
-    } else {
-      mobileNav.classList.remove('open');
-    }
   };
 
   return (
-  
-
-
     <>
       <div className="float-contains">
         <div className="logo">
@@ -39,30 +31,47 @@ const Header = () => {
             <img src={phoneIcon} alt="Phone Icon" />
             <span>+91 9876543210</span>
           </div>
-          <button className="apply-btn" onClick={() => { navigate("/ApplicationPage")}}>Apply now</button>
+          <button className="apply-btn" onClick={() => navigate("/ApplicationPage")}>
+            Apply now
+          </button>
         </div>
       </div>
 
-      {/* mobile nav bar */}
-      <div className="mobile-nav">
-            <div className="nav-mobile">
-             <img src={logo} alt="logo"/>
-            </div>
-          
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
-            </div>
-            <div className={`menu-items ${isOpen ? 'show' : ''}`}>
-                <a href="#home">Home</a>
-                <a href="#aboutus">About Us</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact</a>
-            </div>
+      {/* Mobile nav bar */}
+      <div className={`mobile-nav ${isOpen ? 'open' : ''}`}>
+        <div className="nav-mobile">
+          <img src={logo} alt="logo" />
         </div>
 
-    
+        <div className="hamburger-menu" onClick={toggleMenu}>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+          <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+        </div>
+
+        <div id="nav-items" className={`menu-items ${isOpen ? 'show' : ''}`}>
+                <HashLink
+                  to="/about#about-us"
+                  className="text-decoration-none"
+                >
+                  <a className="nav-link  mx-4">About us</a>
+                </HashLink>
+                <HashLink
+                  to="/services#services"
+                  className="text-decoration-none"
+                >
+                   <a className="nav-link  mx-4">Services</a>
+                </HashLink>
+               
+                <a className="nav-link  mx-4">Careers</a>
+                <HashLink
+                  to="/contactus#contact-us"
+                  className="text-decoration-none"
+                >
+                   <a className="nav-link mx-4">Contact us</a>
+                </HashLink>
+        </div>
+      </div>
     </>
   );
 };
